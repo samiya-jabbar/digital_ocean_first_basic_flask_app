bba38d8d7e067d351c4bb8e5b64fce921919cc92
Samiya Jabbar <samiya.programming@gmail.com>
2021-02-26 15:23:07 +0500

    py code for calculator

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify , request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello!'
+def hello():
+    return "<h1>WELCOME TO THE WORLD OF MATHEMATICS<h1>"
+
+@app.route('/add/<int:usernumber1>/<int:usernumber2>')
+def add(usernumber1, usernumber2):
+    a= usernumber1+usernumber2
+    return "The sum of these two numbers is %s" % a
+
+@app.route('/multiply/<int:usernumber1>/<int:usernumber2>')
+def multiply(usernumber1, usernumber2):
+    a= usernumber1*usernumber2
+    return "The product of these two numbers is %s" % a
+
+@app.route('/divide/<int:usernumber1>/<int:usernumber2>')
+def divide(usernumber1, usernumber2):
+    a= usernumber1/usernumber2
+    return "The result of these two numbers is %s" % a
+
+@app.route('/sub/<int:usernumber1>/<int:usernumber2>')
+def sub(usernumber1, usernumber2):
+    a= usernumber1-usernumber2
+    return "The difference of these two numbers is %s" % a
+
+if __name__=="__main__":
+    app.run()
